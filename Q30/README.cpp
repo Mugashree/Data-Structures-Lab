370caf5d407e3b46f81290133205ed54839fcb71
Mugashree <mugashree73@gmail.com>
2025-05-03 21:50:55 +0530

    Create README.cpp

---
+++ Q30/README.cpp
@@ -0,0 +1,10 @@
+Write a separate C++ menu-driven program to implement Hash ADT with Separate Chaining. Maintain proper boundary conditions and follow good coding practices. The Hash ADT has the following operations:
+
+
+1. Insert
+2. Delete
+3. Search
+4. Exit
+
+
+What is the time complexity of each of the operations? (K4)
